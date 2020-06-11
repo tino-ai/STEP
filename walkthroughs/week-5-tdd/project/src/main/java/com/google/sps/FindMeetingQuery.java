bfdb787fe23a52454273692839a93a3a39cdf5dc
Valentino <tinodore@gmail.com>
2020-06-11 16:58:58 +0000

    Fix and Add Comments. Fix variable names. Remove edge cases from outside general algorithm

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -21,53 +21,52 @@ import java.util.Arrays;
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
     Collection<TimeRange> times = new ArrayList<TimeRange>();
-    int beginning = TimeRange.START_OF_DAY;
-    int end = TimeRange.END_OF_DAY;
-    TimeRange middle_current;
-    TimeRange middle_past = TimeRange.fromStartDuration(TimeRange.END_OF_DAY, 0);
-    //check for primary edge cases of duration longer than one day
-    if (request.getDuration() > (24*60)){
-        return Arrays.asList();
-    //check for primary edge cases of empty attendee list
-    } else if (request.getAttendees().isEmpty()) {
-        return Arrays.asList(TimeRange.WHOLE_DAY);
-    //loop through events updating middle and beginning times
-    //think of it as a line .----------. each time an event is added 
-    //we add a new dot and update the beginnign and middle pointers
-    // .-----.-----.
-    } else {
-        for (Event event : events) {
-            middle_current = event.getWhen();
-            //check if the two events are overlapped
-            if (beginning > middle_current.start()) {
-                //if they are overlapped then make the next avaliable slot
-                //the time of whichever ends later
-                if (middle_current.end() > middle_past.end()) {
-                    beginning = middle_current.end();
-                } else {
-                    beginning = middle_past.end();
-                }
-            //if they aren't overlapped then just update middle and beginning point
+    int start_of_timeslot = TimeRange.START_OF_DAY;
+    int end_of_timeslot = TimeRange.END_OF_DAY;
+    TimeRange middle_current_timeslot;
+    TimeRange middle_past_timeslot = TimeRange.fromStartDuration(TimeRange.END_OF_DAY, 0);
+    for (Event event : events) {
+        // Loop through events updating middle and beginning times
+        // Think of it as a line .----------. each time an event is added 
+        // We add a new dot and update the beginnign and middle pointers
+        // .-----.-----.
+        // Start of timeslot is the beginning of an avaliable period
+        // End of timeslot is the latest possible period something can run to
+        // Middle current timeslot is the current event we are checking to add in points to our timeline
+        // Middle past timeslot was the previous event we checked and added to our timeline
+        middle_current_timeslot = event.getWhen();
+        // Check if the two events are overlapped
+        if (start_of_timeslot > middle_current_timeslot.start()) {
+            // If they are overlapped then make the next avaliable slot
+            // The time of whichever ends later
+            if (middle_current_timeslot.end() > middle_past_timeslot.end()) {
+                start_of_timeslot = middle_current_timeslot.end();
             } else {
-                //make sure the request and the meetings Attendees are the same
-                if (request.getAttendees().containsAll(event.getAttendees()) == false) {
-                    return Arrays.asList(TimeRange.WHOLE_DAY);
-                }
-                TimeRange new_time = TimeRange.fromStartEnd(beginning, middle_current.start(), false);
-                if (new_time.duration() >= request.getDuration()) {
-                    times.add(new_time);
-                }
-                beginning = middle_current.end();
+                start_of_timeslot = middle_past_timeslot.end();
             }
-            middle_past = event.getWhen();
+        // If they aren't overlapped then just update middle and beginning point
+        } else {
+            // Make sure the request and the meetings Attendees are the same
+            if (request.getAttendees().containsAll(event.getAttendees()) == false) {
+                return Arrays.asList(TimeRange.WHOLE_DAY);
+            }
+            TimeRange new_timeslot = TimeRange.fromStartEnd(start_of_timeslot, middle_current_timeslot.start(), false);
+            if (new_timeslot.duration() >= request.getDuration()) {
+                times.add(new_timeslot);
+            }
+            start_of_timeslot = middle_current_timeslot.end();
         }
-        //get the final interval from ending of last meeting to end of day
-        TimeRange final_time = TimeRange.fromStartEnd(beginning, end, true);
-        //make sure the intervals are more than 0 minutes
-        if (final_time.duration() > 0) {
-            times.add(final_time);
-        }
-        return times;
+        middle_past_timeslot = event.getWhen();
     }
+    // Get the final interval from ending of last meeting to end of day
+    TimeRange final_time = TimeRange.fromStartEnd(start_of_timeslot, end_of_timeslot, true);
+    // Make sure the intervals are more than 0 minutes
+    if (final_time.duration() > 0) {
+        times.add(final_time);
+    }
+    if (request.getDuration() > TimeRange.WHOLE_DAY.duration()) {
+        return Arrays.asList();
+    }
+    return times;
   }
 }
